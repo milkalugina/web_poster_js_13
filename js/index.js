989a0a0f6e9cb185675e4f9f4940ba6e1644fb89
Mila Kalugina <milkalugin@gmail.com>
2020-03-13 12:26:30 +0300

    исправила скролл

--- js/index.js
+++ js/index.js
@@ -5,10 +5,10 @@ let $box2 = document.getElementById('main_cat')
 
 let box2Anim = anime({
 targets: $box2,
-height: ['20vw', '1170vw'],
-width: ['20vw', '1170vw'],
-left: ['40vw', '-595vw'],
-top: ['180px', '-9000px'],
+height: ['20vw', '1223vw'],
+width: ['20vw', '1223vw'],
+left: ['40vw', '-534vw'],
+top: ['220px', '-9000px'],
 autoplay: false,
 easing: 'linear'
 })
