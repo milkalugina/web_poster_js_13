4d23caa705a774d09a793493e1ea9c1572e0e76b
Mila Kalugina <milkalugin@gmail.com>
2020-03-13 23:28:32 +0300

    добавила блок с "падающим котом"

--- js/index.js
+++ js/index.js
@@ -24,3 +24,15 @@ duration: $section2.getBoundingClientRect().height
 .on('progress', function(event) {
 box2Anim.seek(box2Anim.duration * event.progress)
 })
+
+document.getElementById('btn_white').onclick = function() { document.getElementById('btn_black').style.display = 'block';
+document.getElementById('cat_on').style.display = 'block'
+document.getElementById('cat_on').style.animation = `aniCat 1s ease-in 1 forwards`
+ }
+
+
+document.getElementById('btn_black').onclick = function() {
+document.getElementById('btn_black').style.display = 'none'
+document.getElementById('cat_on').style.display = 'none'
+document.getElementById('cat_on').style.animation = `opacityCat 3s ease-in-out 1 forwards`
+}
