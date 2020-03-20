da448be82b491876b5157fd48ce3a45e6a0c43aa
Mila Kalugina <milkalugin@gmail.com>
2020-03-20 17:56:07 +0300

    разобралась со скролом и сделала 2 секцию

--- js/index.js
+++ js/index.js
@@ -5,12 +5,12 @@ let $box2 = document.getElementById('main_cat')
 
 let box2Anim = anime({
 targets: $box2,
-height: ['20vw', '1223vw'],
-width: ['20vw', '1223vw'],
-left: ['40vw', '-534vw'],
-top: ['220px', '-9000px'],
+// height: ['0vw', '300vh'],
+scale: 90,
+// left: ['50%', '50%'],
+// top: ['20px', '-50%'],
 autoplay: false,
-easing: 'linear'
+easing: 'easeInSine'
 })
 
 let $section2 = document.getElementById('first')
@@ -22,7 +22,7 @@ duration: $section2.getBoundingClientRect().height
 .addTo(controller)
 .setPin($box2)
 .on('progress', function(event) {
-box2Anim.seek(box2Anim.duration * event.progress)
+  box2Anim.seek(box2Anim.duration * (event.progress * 1))
 })
 
 document.getElementById('btn_white').onclick = function() { document.getElementById('btn_black').style.display = 'block';
@@ -35,4 +35,92 @@ document.getElementById('btn_black').onclick = function() {
 document.getElementById('btn_black').style.display = 'none'
 document.getElementById('cat_on').style.display = 'none'
 document.getElementById('cat_on').style.animation = `opacityCat 3s ease-in-out 1 forwards`
+}
+
+
+document.getElementById('museum_cup').onclick = function(){
+document.getElementById('museum_cup').style.animation = `fjfj 0.5s ease-in-out 1 forwards`
+}
+
+document.getElementById('museum').onclick = function(){
+document.getElementById('museum_cup').style.animation = `jf 0.5s ease-in-out 1 forwards`
+}
+
+document.getElementById('home_cup').onclick = function(){
+document.getElementById('home_cup').style.animation = `fjfj 0.5s ease-in-out 1 forwards`
+}
+
+document.getElementById('primetnaya_cup').onclick = function(){
+document.getElementById('primetnaya_cup').style.animation = `fjfj 0.5s ease-in-out 1 forwards`
+}
+
+document.getElementById('omen_cup').onclick = function(){
+document.getElementById('omen_cup').style.animation = `fjfj 0.5s ease-in-out 1 forwards`
+}
+
+document.getElementById('time_cup').onclick = function(){
+document.getElementById('time_cup').style.animation = `fjfj 0.5s ease-in-out 1 forwards`
+}
+
+
+
+document.getElementById('cat_first').onclick = function(){
+document.getElementById('second').style.backgroundColor = `white`;
+document.getElementById('cats_all').style.display = 'block'
+}
+
+document.getElementById('cats_all').onclick = function(){
+document.getElementById('second').style.backgroundColor = `black`;
+document.getElementById('cat_first').style.display = 'none'
+document.getElementById('cats_all_negative').style.display = 'block'
+document.getElementById('cats_all').style.display = 'none'
+}
+
+document.getElementById('cats_all_negative').onclick = function(){
+document.getElementById('second').style.backgroundColor = `white`;
+document.getElementById('cats_all_negative').style.display = 'none'
+document.getElementById('cat_first').style.display = 'none'
+document.getElementById('cats_all').style.display = 'block'
+}
+
+// Сколько создавать карт
+const CARDS_NUMBER = 10
+// Название файлов с картами
+let cards = [
+  '../img/1_card.svg',
+  '../img/2_card.svg',
+  '../img/3_card.svg',
+  '../img/4_card.svg',
+  '../img/5_card.svg',
+  '../img/6_card.svg',
+  '../img/7_card.svg',
+  '../img/8_card.svg',
+  '../img/9_card.svg',
+  '../img/10_card.svg',
+  '../img/11_card.svg',
+  '../img/12_card.svg',
+  '../img/13_card.svg'
+]
+// Функция, которая перемешивает массив с названиямии
+function shuffle(cards) {
+  for (let i = cards.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [cards[i], cards[j]] = [cards[j], cards[i]];
+  }
+}
+// Применяем функцию перемешивания
+cards = shuffle(cards)
+
+// Находим контейнер для карт
+// Он нам нужен, чтобы в ЦСС все выровнять
+let cardsContainer = document.getElementById('third__cards-container')
+// При помощи цикла создаем карты и вставляем внутрь контейнера
+for (let i = 0; i < CARDS_NUMBER; i++) {
+  let newCard = document.createElement('img')
+  newCard.src = `img/${cards[i]}`
+  newCard.classList.add('card')
+  newCard.onclick = function() {
+
+  }
+  cardsContainer.append(newCard)
 }
